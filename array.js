0a0f4d872103b34331f8f885af9fe68b61bff5e5
jv-nunes <jvnlino@gmail.com>
2024-01-19 15:37:31 -0300

    teste

---
+++ array.js
@@ -0,0 +1,13 @@
+let gaveteiro = ["joao", "eduardo", "dani", "sunki"]
+gaveteiro.push("lino")
+//push é pra adicionar no final
+console.log(gaveteiro)
+gaveteiro.pop()
+//pop é pra excluir a ultima posição
+console.log(gaveteiro)
+gaveteiro.shift()
+//shift exclui a 1 posi.
+console.log(gaveteiro)
+gaveteiro.unshift("joao")
+//unshift add na 1 posi.
+console.log(gaveteiro)
---
+++ paises.js
@@ -0,0 +1,28 @@
+let continente = ["reino unido", "noruega", "frança", "grecia", "letônia"]
+continente.push("alemanha") 
+console.log(continente)
+
+continente.pop()
+console.log(continente)
+
+continente.unshift("suiça")
+console.log(continente)
+
+continente.shift()
+console.log(continente)
+//console.log(continente.length)
+
+let last = continente.pop()
+console.log(last)
+// e se eu quisesse que ele imprima o último da lista atual?
+
+//console.log(continente[1])                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                  
+
+console.log(continente.length)
+console.log(continente[continente.length - 1])
+
+let x = 3
+console.log(continente[x])
+
+
+
